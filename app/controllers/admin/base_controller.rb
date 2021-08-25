792204049e1420480a5d3defdf7ebd5b14c61ff1
Zach T <ztrokey@gmail.com>
2021-08-25 11:28:08 -0600

    nothing

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,4 +4,8 @@ class Admin::BaseController < ApplicationController
   def require_admin
     render file: 'public/404.html', status: 404, layout: false unless current_admin?
   end
+
+  def nothing
+    render file: 'this_doesnt_do_anything'
+  end
 end
